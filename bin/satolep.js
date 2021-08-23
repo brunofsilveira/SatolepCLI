db40440b93911cb791605f16f5a69c6e1b11880f
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2021-08-23 03:24:55 -0300

    ✨ Add to 'satolep' command

---
+++ bin/satolep.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+/**
+ * @fileoverview Main CLI that is run via the satolep command.
+ * @author Bruno Ferraz Silveira
+ */
+
+import '../src/index.js'
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import generateEslintrc from './scripts/generateEslintrc.js'
 import selectLanguage from './scripts/selectLanguage.js'
 import selectPackageManager from './scripts/selectPackageManager.js'
 
-export default async function run() {
+export default async function app() {
   const language = await selectLanguage()
   const packageManager = await selectPackageManager()
 
